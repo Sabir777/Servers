81fe8aa19c561760b32a1d2369c4117b645777c1
Sabir777 <sabir.yanin2014@yandex.ru>
2025-04-19 02:33:27 +0300

    Реализовал монтирование удаленной папки на сервере nextcloud для WSL2

--- сервер_2_nextcloud/mnt_nextcloud_scripts/mnt_user2_nextcloud.sh
+++ сервер_2_nextcloud/mnt_nextcloud_scripts/mnt_user2_nextcloud.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # размонтирую папку, чтобы устранить ошибки
-umount ~/mnt_user2_nextcloud
+umount ~/Server_connect/mnt_user2_nextcloud
 
 # монтирую папку с сервера
-sshfs user2@85.159.231.218:/home/user2 ~/mnt_user2_nextcloud
+sshfs user2@85.159.231.218:/home/user2 ~/Server_connect/mnt_user2_nextcloud
 
